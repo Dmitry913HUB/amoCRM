fe3199ad7d556d4e6a6f087dc1a26451a5615142
Dmitry913HUB <58403614+Dmitry913HUB@users.noreply.github.com>
2023-03-18 00:53:57 +0300

    timer completed

--- 1 task/index.js
+++ 1 task/index.js
@@ -1,23 +1,35 @@
 const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
+let timerId;
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    timerId = setInterval(() =>{
+      let h = Math.floor(seconds / 3600);
+      let m = Math.floor((seconds - h * 3600) / 60);
+      let s = Math.floor((seconds - h * 3600 - m * 60));
+
+      timerEl.textContent = `${zeroPad(h, 2)}:${zeroPad(m, 2)}:${zeroPad(s, 2)}`;
+      seconds--;
+
+      if(0 >= seconds) clearInterval(timerId);
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
+  inputEl.value = inputEl.value.replace(/\D/g,'');
 });
 
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
 
+  if(timerId) clearInterval(timerId);
   animateTimer(seconds);
 
   inputEl.value = '';
